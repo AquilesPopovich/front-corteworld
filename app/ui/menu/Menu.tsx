1512d96e9308c5f45772ff465d8319b05afabc0c
Aquiles Popovich <popovichaquiles@gmail.com>
2024-04-13 22:23:04 -0300

    navbar

--- app/ui/menu/Menu.tsx
+++ app/ui/menu/Menu.tsx
@@ -1,36 +1,52 @@
-import Link from "next/link"
-import Image from "next/image"
+import Link from 'next/link';
+import Image from 'next/image';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import styles from './menu.module.css';
 import corteWorld from '../images/corteWorld.png';
 
 export const Menu = () => {
+  return (
+    <nav className="bg-gray-800 text-white flex justify-between items-center p-4">
+      <div className="flex items-center">
+        {/* Logo */}
+        <div className="mr-4">
+          <Link href="/">
+            <Image src={corteWorld} alt="CorteWorld" width={70} height={70} />
+          </Link>
+        </div>
+        </div>
 
-    return(
-        <>
-            <nav className=" bg-white flex justify-evenly md:items-center">
-                <div>
-                    <Image src={corteWorld} alt="CorteWorld"/>
-                </div>
-                <Link href='/products'>Productos</Link>
-                <div>
-                <Link href='/favorites'>Favoritos</Link>
-                </div>
-                <div>
-                <Link href='/carrito'>Carrito</Link>
-                </div>
-                <div>
-                    <Link href='/historial'>Historial</Link>
-                </div>
-                <div>
-                    <input type="text"
-                    placeholder="Search a product..."
-                    // value=''
-                    className=" bg-white border-x-2 border-y-2 border-neutral-800 mr-4"
-                    />
-                    <button
-                    className=" bg-white border-x-2 border-y-2 border-neutral-800">
-                        Search</button>
-                </div>
-            </nav>
-        </>
-    )
-}
+        {/* SearchBar */}
+        <div className={`flex items-center ${styles.searchContainer}`}>
+          <SearchIcon className={styles.searchIcon} />
+          <input
+            type="text"
+            placeholder="Busca un producto"
+            className={`${styles.searchInput} text-black`}
+            style={{ fontSize: '13px', paddingLeft: '15px' }}
+          />
+        
+      </div>
+
+      {/* Enlaces con iconos */}
+      <div className="flex items-center">
+        <div className="ml-4 flex items-center">
+          <Image src={corteWorld} alt="Productos" width={20} height={20} />
+          <Link href="/products" className={`ml-2 ${styles.link}`}>Productos</Link>
+        </div>
+        <div className="ml-4 flex items-center">
+          <Image src={corteWorld} alt="Favoritos" width={20} height={20} />
+          <Link href="/favorites" className={`ml-2 ${styles.link}`}>Favoritos</Link>
+        </div>
+        <div className="ml-4 flex items-center">
+          <ShoppingCartOutlinedIcon />
+        </div>
+        <div className="ml-4 flex items-center">
+          <Image src={corteWorld} alt="Historial" width={20} height={20} />
+          <Link href="/historial" className={`ml-2 ${styles.link}`}>Historial</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
