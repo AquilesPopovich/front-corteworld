0748e7b8d883243f11d332c60c76ea8d3981e159
Aquiles Popovich <popovichaquiles@gmail.com>
2024-04-14 14:30:26 -0300

    fn agregar y eliminar favorito

---
+++ redux/features/favoriteSlice.ts
@@ -0,0 +1,42 @@
+'use client'
+
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { useState } from "react";
+
+type Product = {
+    id: number
+    name: string
+    price: number
+    mark: string
+    status: string 
+    stock: number
+    discount: number
+    category: string
+    destacado: boolean
+  };
+
+
+interface FavoritesState {
+    favorites: Product[];
+}
+
+const initialState: FavoritesState = {
+    favorites: []
+};
+
+export const favoritesSlice = createSlice({
+    name: "favorites",
+    initialState,
+    reducers: {
+        addFavorite: (state, action: PayloadAction<Product>) =>{
+            state.favorites.push(action.payload);
+        },
+        removeFavorite: (state, action: PayloadAction<number>) => {
+            state.favorites = state.favorites.filter(product => product.id !== action.payload);
+        }
+    }
+})
+
+export const { addFavorite, removeFavorite} = favoritesSlice.actions
+
+export default favoritesSlice.reducer
