b85a21be1c3f1f3eaf89651c6be17e38fadb6ce8
Aquiles Popovich <popovichaquiles@gmail.com>
2024-04-14 15:04:27 -0300

    modificaciones detail

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -76,56 +76,59 @@ const DetailPage = () => {
         <>
         <Menu />
         <div className="container mx-auto mt-8" style={{ marginTop: '120px' }}>
-            <div className="grid grid-cols-1 md:grid-cols-1 gap-4"> 
-                <div className="w-1/12 md:w-1/6"> {/* Reducir ancho de este div */}
-                    {producto.imgs.map((img, index) => (
-                        <img
-                            key={index}
-                            src={img}
-                            alt={`Imagen ${index}`}
-                            className={`w-20 h-auto rounded-lg shadow-md cursor-pointer ${selectedImg === index ? 'border-2 border-blue-500' : 'mr-2'}`}
-                            onClick={() => handleImgClick(index)}
-                        />
-                    ))}
-                </div>
-                <div className="w-11/12 md:w-2/6">
-                    <img
-                        src={producto.imgs[selectedImg]}
-                        alt={`Imagen grande ${selectedImg}`}
-                        className="w-full h-auto rounded-lg shadow-md mb-4 md:float-right"
-                    />
-                </div>
-
-                    <div> {/* Este div estará al lado de la imagen grande (a la derecha) */}
-                        <h1 className="text-3xl font-bold mb-4">{producto.name}</h1>
-                        <p className="text-lg mb-2">Marca: {producto.mark}</p>
-                        <p className="text-lg mb-2">Stock: {producto.stock}</p>
-                        <p className="text-lg mb-2">Categoría: {producto.category}</p>
-                        <p className="text-lg mb-4">Precio: ${producto.price}</p>
-                        <div className="flex items-center mb-4">
-                            <label htmlFor="cantidad" className="mr-2">Cantidad:</label>
-                            <button onClick={reducirCantidad} className="bg-pink-400 hover:bg-pink-700 text-white py-1 px-3 rounded-md  focus:outline-none">-</button>
-                            <span className="mx-2">{cantidad}</span>
-                            <button onClick={aumentarCantidad} className="bg-pink-400 hover:bg-pink-700 text-white py-1 px-3 rounded-md  focus:outline-none">+</button>
-                        </div>
-                        <button onClick={agregarAlCarrito} className="bg-pink-400 hover:bg-pink-700 text-white py-2 px-4 rounded-md  focus:outline-none">Agregar al carrito</button>
-                    </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2"> {/* Reducir el espacio entre las columnas */}
+    <div className="w-1/5 md:w-1/6 flex flex-col items-center"> {/* Reducir el ancho y el espacio alrededor */}
+        {producto.imgs.map((img, index) => (
+            <img
+                key={index}
+                src={img}
+                alt={`Imagen ${index}`}
+                className={`w-20 h-auto rounded-lg shadow-md cursor-pointer ${selectedImg === index ? 'border-2 border-blue-500' : 'mr-2'}`}
+                onClick={() => handleImgClick(index)}
+            />
+        ))}
+    </div>
+    <div className="w-2/5 md:w-1/2"> {/* Reducir el ancho */}
+        <img
+            src={producto.imgs[selectedImg]}
+            alt={`Imagen grande ${selectedImg}`}
+            className="w-full h-auto rounded-lg shadow-md mb-4 md:float-right"
+        />
+    </div>
+    <div className="w-2/5 md:w-2/3 flex flex-col justify-between"> {/* Aumentar el ancho */}
+        <div>
+            <h1 className="text-3xl font-bold mb-4">{producto.name}</h1>
+            <p className="text-lg mb-2">Marca: {producto.mark}</p>
+            <p className="text-lg mb-2">Stock: {producto.stock}</p>
+            <p className="text-lg mb-2">Categoría: {producto.category}</p>
+            <p className="text-lg mb-4">Precio: ${producto.price}</p>
+            <div className="flex items-center mb-4">
+                <label htmlFor="cantidad" className="mr-2">Cantidad:</label>
+                <button onClick={reducirCantidad} className="bg-pink-400 hover:bg-pink-700 text-white py-1 px-3 rounded-md  focus:outline-none">-</button>
+                <span className="mx-2">{cantidad}</span>
+                <button onClick={aumentarCantidad} className="bg-pink-400 hover:bg-pink-700 text-white py-1 px-3 rounded-md  focus:outline-none">+</button>
             </div>
-            <div className="mt-8" style={{ maxHeight: '300px', overflowY: 'auto' }}>
+            <button onClick={agregarAlCarrito} className="bg-pink-400 hover:bg-pink-700 text-white py-2 px-4 rounded-md  focus:outline-none">Agregar al carrito</button>
+        </div>
+    </div>
+</div>
+
+
+            <div className="w-full mt-8" style={{ maxHeight: '300px', overflowY: 'auto' }}>
                 <h2 className="text-2xl font-bold mb-4">Comentarios</h2>
                 {comentarios.map((comentario, index) => (
                     <div key={index} className="flex mb-4 items-start">
                         <div className="w-8 h-8 bg-gray-200 flex items-center justify-center rounded-full mr-4">
                             <Chat />
                         </div>
-                        <div className="bg-gray-100 p-4 rounded-lg">
+                        <div className="bg-gray-100 p-4 rounded-lg w-full">
                             <p className="text-lg font-semibold">{comentario.name}</p>
                             <p className="text-lg">{comentario.comentario}</p>
                         </div>
                     </div>
                 ))}
             </div>
-            <form onSubmit={agregarComentario} className="mt-8">
+            <form onSubmit={agregarComentario} className="w-full mt-8">
                 <label htmlFor="comentario" className="block text-lg font-semibold mb-2">Deja tu comentario:</label>
                 <textarea
                     id="comentario"
@@ -138,6 +141,8 @@ const DetailPage = () => {
             </form>
         </div>
     </>
+    
+    
     
     
 
