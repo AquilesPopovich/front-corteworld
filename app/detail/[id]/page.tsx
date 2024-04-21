c21de9f0fb029f9bd7a7eb2880f2dc61c4575873
Aquiles Popovich <popovichaquiles@gmail.com>
2024-04-21 13:31:34 -0300

    mas detail

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useEffect, useState } from 'react';
 import { Chat } from '@mui/icons-material';
 import { Menu } from '@/app/ui/menu/Menu';
@@ -10,74 +9,43 @@ import { useAppSelector } from '@/redux/hook';
 import axiosURL from '@/axiosConfig/axiosConfig';
 
 const DetailPage = () => {
-    // const producto = {
-    //     id: 3,
-    //     imgs: [
-    //         'https://res.cloudinary.com/dphpu225t/image/upload/v1708733825/teclado_dx8rko.png',
-    //         'https://res.cloudinary.com/dphpu225t/image/upload/v1708733824/mouse_pubjux.png',
-    //         'https://res.cloudinary.com/diswtvj50/image/upload/v1708807040/cpu3_fcg4as.png'
-    //     ],
-    //     name: 'CPU Gamer',
-    //     mark: 'Corsair',
-    //     stock: 10,
-    //     category: 'buzos',
-    //     price: 1500,
-    //     comentarios: [
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'lucas', comentario: 'mal producto :c' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //         { name: 'javier', comentario: 'buen producto' },
-    //     ]
-    // };
     const [producto, setProducto] = useState(null);
-    const [imagenes, setImagenes] = useState([])
-    const [comentario, setComentario] = useState([])
-
+    const [imagenes, setImagenes] = useState([]);
+    const [comentario, setComentario] = useState('');
+    const [comentarios, setComentarios] = useState([]);
+    const [selectedImg, setSelectedImg] = useState(0);
+    const [cantidad, setCantidad] = useState(1);
+    const user = useAppSelector(state => state.userSlice.user);
     const { id } = useParams();
+    const dispatch = useDispatch();
 
-    
     useEffect(() => {
         const fetchData = async () => { 
             try {
                 const { data } = await axios(`/products/${id}`);
-                if (!data) throw new Error('No se encontraron datos para el producto');
-                setProductoDetail(data);
-                const {dataImg} = await axios(`/imgProduct/${id}`)
-                if(dataImg) setImagenes(dataImg)
-                const {dataComentario} = await axios(`/comentarios/${id}`)
-                if(dataComentario) setComentario(dataComentario)
-
-            } catch (error: unknown) {
+                if(data){
+                    setProducto(data);
+                    const { dataImg } = await axios(`/imgProduct/${id}`);
+                    if (dataImg) setImagenes(dataImg);
+                    const { dataComentario } = await axios(`/comentarios/${id}`);
+                    if (dataComentario) setComentarios(dataComentario);
+                }
+              
+            } catch (error: any) {
                 console.error('Error al obtener los datos del producto:', error.message);
             }
         };
         fetchData();
-    }, []);
+    }, [id]);
 
-    const dispatch= useDispatch()
-  const user = useAppSelector(state => state.userSlice.user);
-
-    
     const handleAgregarCarrito = () => {
-        if(!user) alert('necesitas loguearte para agregar un producto al carrito')
-        dispatch(agregarCarrito({ id, name, img, mark, price }));
-      };
-    
-
-    const [selectedImg, setSelectedImg] = useState(0); // Estado para almacenar la imagen seleccionada
+        if (!user) alert('Necesitas iniciar sesión para agregar un producto al carrito');
+        dispatch(agregarCarrito({ id, name: producto.name, img: imagenes[0], mark: producto.mark, price: producto.price }));
+    };
 
     const handleImgClick = (index) => {
         setSelectedImg(index);
     };
-
-    const [cantidad, setCantidad] = useState(1);
-    const [comentario, setComentario] = useState('');
-    const [comentarios, setComentarios] = useState(producto.comentarios);
-
 
     const handleComentarioChange = (e) => {
         setComentario(e.target.value);
@@ -93,12 +61,16 @@ const DetailPage = () => {
         }
     };
 
-    const agregarComentario = (e) => {
+    const agregarComentario = async (e) => {
         e.preventDefault();
         if (comentario.trim() !== '') {
-            const nuevoComentario = { name: 'Usuario', comentario: comentario };
-            setComentarios([...comentarios, nuevoComentario]);
-            setComentario('');
+            try {
+                const nuevoComentario = { userId: user[0]?.user?.id, comentario: comentario, productId: id };
+                const { data } = await axios.post(`/comentarios`, nuevoComentario);
+                setComentarios([...comentarios, data]);
+            } catch (error) {
+                console.error('Error al agregar comentario:', error.message);
+            }
         }
     };
 
@@ -123,20 +95,20 @@ const DetailPage = () => {
                     </div>
                     <div className="w-full md:w-1/2">
                         <Image
-                            src={imagenes?[selectedImg]}
+                            src={imagenes[selectedImg]}
                             alt={`Imagen grande ${selectedImg}`}
-                            width={500} // Define el ancho deseado para la miniatura de la imagen
+                            width={500}
                             height={500}
                             className="w-full h-auto rounded-lg shadow-md mb-4 md:float-right"
                         />
                     </div>
                     <div className="w-full md:w-2/3 flex flex-col justify-between">
                         <div>
-                            <h1 className="text-3xl font-bold mb-4">{producto.name}</h1>
-                            <p className="text-lg mb-2">Marca: {producto.mark}</p>
-                            <p className="text-lg mb-2">Stock: {producto.stock}</p>
-                            <p className="text-lg mb-2">Categoría: {producto.category}</p>
-                            <p className="text-lg mb-4">Precio: ${producto.price}</p>
+                            <h1 className="text-3xl font-bold mb-4">{producto?.name}</h1>
+                            <p className="text-lg mb-2">Marca: {producto?.mark}</p>
+                            <p className="text-lg mb-2">Stock: {producto?.stock}</p>
+                            <p className="text-lg mb-2">Categoría: {producto?.category}</p>
+                            <p className="text-lg mb-4">Precio: ${producto?.price}</p>
                             <div className="flex items-center mb-4">
                                 <label htmlFor="cantidad" className="mr-2">Cantidad:</label>
                                 <button onClick={reducirCantidad} className="bg-pink-400 hover:bg-pink-700 text-white py-1 px-3 rounded-md focus:outline-none">-</button>
@@ -148,9 +120,7 @@ const DetailPage = () => {
                     </div>
                 </div>
 
-
-
-                    <h2 className="text-2xl font-bold mb-4 ml-5">Comentarios</h2>
+                <h2 className="text-2xl font-bold mb-4 ml-5">Comentarios</h2>
                 <div className="w-full mt-8 ml-11" style={{ maxHeight: '300px', overflowY: 'auto' }}>
                     {comentarios.map((comentario, index) => (
                         <div key={index} className="flex mb-4 items-start">
@@ -177,13 +147,6 @@ const DetailPage = () => {
                 </form>
             </div>
         </>
-
-
-
-
-
-
-
     );
 }
 
