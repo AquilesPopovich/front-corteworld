82989d49dad72e688ab3dd583df51a846c1ba964
Aquiles Popovich <popovichaquiles@gmail.com>
2024-04-21 23:27:24 -0300

    localstorage

--- redux/features/carritoSlice.ts
+++ redux/features/carritoSlice.ts
@@ -1,30 +1,40 @@
 'use client'
+import { Products } from "@/app/types/typeProduct";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-import { Products } from "@/app/types/typeProduct"
-import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+// Definir una clave para el localStorage
+const CARRITO_STORAGE_KEY = 'carrito';
+
+// Obtener el carrito del localStorage si está disponible
+const carritoFromLocalStorage = localStorage.getItem(CARRITO_STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(CARRITO_STORAGE_KEY)!)
+  : [];
 
 interface Carrito {
-    carrito: Products[]
+    carrito: Products[];
 }
 
 const initialState: Carrito = {
-    carrito: []
-}
+    carrito: carritoFromLocalStorage,
+};
 
 export const CarritoSlice = createSlice({
     name: 'carrito',
     initialState,
     reducers: {
         agregarCarrito: (state, action: PayloadAction<Products>) => {
-            state.carrito.push(action.payload)
+            state.carrito.push(action.payload);
+            // Guardar el estado del carrito en localStorage después de agregarlo
+            localStorage.setItem(CARRITO_STORAGE_KEY, JSON.stringify(state.carrito));
         },
-        removeCarrito: (state, action: PayloadAction<string>) =>{
-            state.carrito = state.carrito.filter(producto => producto.id !== action.payload)
+        removeCarrito: (state, action: PayloadAction<string>) => {
+            state.carrito = state.carrito.filter(producto => producto.id !== action.payload);
+            // Guardar el estado del carrito en localStorage después de eliminar un producto
+            localStorage.setItem(CARRITO_STORAGE_KEY, JSON.stringify(state.carrito));
         }
     }
-})
+});
 
-export const { agregarCarrito, removeCarrito} = CarritoSlice.actions
+export const { agregarCarrito, removeCarrito } = CarritoSlice.actions;
 
-export default CarritoSlice.reducer
-
+export default CarritoSlice.reducer;
--- redux/features/userSlice.ts
+++ redux/features/userSlice.ts
@@ -1,23 +1,36 @@
+'use client'
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { User, UserList } from "@/app/types/typeUser";
+
+// Definir una clave para el localStorage
+const USER_STORAGE_KEY = 'user';
+
+// Obtener el usuario del localStorage si está disponible
+const userFromLocalStorage = localStorage.getItem(USER_STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(USER_STORAGE_KEY)!)
+  : [];
 
 interface InitialState {
     user: UserList
 }
 
 const initialState: InitialState = {
-    user: []
+    user: userFromLocalStorage,
 };
 
 export const userSlice = createSlice({
     name: 'user',
-    initialState,
+    initialState: initialState,
     reducers: {
         agregarUser: (state, action: PayloadAction<User>) => {
             state.user.push(action.payload);
+            // Guardar el estado del usuario en localStorage después de agregarlo
+            localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(state.user));
         },
         logOutUser: (state) => {
             state.user.pop();
+            // Guardar el estado del usuario en localStorage después de cerrar sesión
+            localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(state.user));
         }
     }
 });
