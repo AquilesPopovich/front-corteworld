0b3cfba6aabd7d8f28fbf7e5061e9f2361bf81e2
LMellaVillanueva <mellalucas.v@gmail.com>
2024-05-17 15:51:09 -0400

    comentarios responsive

--- app/ui/usuariosDeshabilitados/ComentariosUser/ComentariosUser.tsx
+++ app/ui/usuariosDeshabilitados/ComentariosUser/ComentariosUser.tsx
@@ -1,6 +1,7 @@
 import { Comentario, ComentarioList } from "@/app/types/typeComentario"
 import axiosURL from "@/axiosConfig/axiosConfig"
 import { useEffect, useState } from "react"
+import styles from './comentariosUser.module.css';
 
 interface Props {
     userId: string
@@ -28,15 +29,18 @@ const ComentariosUsers: React.FC<Props> = ({ userId, comentarios, setComentarios
 
     return (
         <div>
-            {comentariosUser.length ? comentariosUser.map((comentario: Comentario) => (
-                <div key={comentario.id} className={`flex`}>
-                    <h3>{comentario.comentario}</h3>
-                    <p>{new Date(comentario.date).toLocaleDateString()}</p>
-                </div>
-            )) : (
-                <p>No hay comentarios</p>
-            )}
-            <button onClick={() => setComentarios([])}>Cancelar</button>
+            <div className={`fixed flex flex-col justify-start items-start gap-6 top-40 right-96 w-2/4 h-3/5 bg-slate-100 p-14 z-10 overflow-y-auto rounded ${styles.container}`}>
+                {comentariosUser.length ? comentariosUser.map((comentario: Comentario) => (
+                    <div key={comentario.id} className={`flex justify-between w-full h-fit text-lg border-b-2 border-black border-dashed flex-wrap ${styles.comentarioContainer}`}>
+                        <h3 className={`w-3/4 break-words font-semibold ${styles.comentario}`}>Comentario: {comentario.comentario}</h3>
+                        <p>{new Date(comentario.date).toLocaleDateString()}</p>
+                    </div>
+                )) : (
+                    <p className={`text-lg`}>No hay comentarios</p>
+                )}
+                <button className=' bg-pink-200 rounded-md hover:bg-pink-400' onClick={() => setComentarios([])}>Cancelar</button>
+            </div>
+            <div className={`fixed top-0 bottom-0 left-0 right-0 w-screen h-screen bg-gray-600 opacity-90`}></div>
         </div>
     )
 }
